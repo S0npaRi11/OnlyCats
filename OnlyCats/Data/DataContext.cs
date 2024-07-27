01a1569e05636d3a44cba1f26b4a27a7bba27eed
s0npaRi11 <paragmahale36@gmail.com>
2024-07-27 11:19:36 +0530

    Add project files.

---
+++ OnlyCats/Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlyCats.Entities;
+using Microsoft.Extensions.Configuration;
+
+namespace OnlyCats.Data
+{
+    public class DataContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        public DataContext(DbContextOptions options, IConfiguration configuration) : base(options)
+        {
+            _configuration = configuration;
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //base.OnConfiguring(optionsBuilder);
+            if (!optionsBuilder.IsConfigured) {
+                optionsBuilder.UseSqlite(_configuration.GetConnectionString("DefaultConnection"));
+            }
+        }
+    }
+}
